9534f3a2b1ae7230d302222aaf6bc6eb5453d7a4
Octavio Olaeta <octavioolaeta28@gmail.com>
2023-07-30 20:46:24 -0300

    pre entrega terminada

--- main.js
+++ main.js
@@ -16,9 +16,19 @@ function numeroAzar(){
 
 elegir.addEventListener("click", function(){
     let elegirNumero = prompt("Eliga un numero del 1 al 6 y se le asignará un color")
-    if (elegirNumero= 1){
-        return (document.body.style.backgroundColor = colores[0]) + (alert("Su color es: Rojo"))
-    }else if(elegirNumero = "2"){
-            return (document.body.style.backgroundColor = colores[1]) + (alert("Su color es: Amarillo"))
+    if (elegirNumero == "1"){
+        return (document.body.style.backgroundColor = colores[0]) + (alert("Su color es: Rojo (#FE2600)"))
+    }else if(elegirNumero == "2"){
+            return (document.body.style.backgroundColor = colores[1]) + (alert("Su color es: Amarillo (#FCF342)"))
+    }else if(elegirNumero == "3"){
+        return (document.body.style.backgroundColor = colores[2]) + (alert("Su color es: Verde (#73A580)"))
+    }else if(elegirNumero == "4"){
+        return (document.body.style.backgroundColor = colores[3]) + (alert("Su color es: Azul (#5189E9)"))
+    }else if(elegirNumero == "5"){
+        return (document.body.style.backgroundColor = colores[4]) + (alert("Su color es: Rosa (#FFB3A5)"))
+    }else if(elegirNumero == "6"){
+        return (document.body.style.backgroundColor = colores[5]) + (alert("Su color es: Blanco (#FFFFFF)"))
+    } else{
+        return alert ("Por favor introduzca alguno de los numeros indicados")
     }
 })
